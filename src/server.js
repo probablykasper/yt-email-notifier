0564bfed949b32be3d27b2d5a87c3f5dd428749b
Kasper <kasperkh.kh@gmail.com>
2020-08-25 23:55:25 +0200

    Ability to add api key and channels (yay)

--- src/server.js
+++ src/server.js
@@ -2,31 +2,32 @@ const http = require('http')
 const connect = require('connect')
 const serveStatic = require('serve-static')
 const WebSocket = require('ws')
-// const fetch = require('node-fetch')
+const fetch = require('node-fetch')
 
 const app = connect()
 app.use(serveStatic('src/web'))
 let server
 
-// async function fetchYT(user, options) {
-//   let url = options.url
-//   if (options.query) {
-//     url += '?'
-//     for (const key in options.query) {
-//       url += key+'='+options.query[key]+'&'
-//     }
-//   }
-//   if (!options.headers) options.headers = {}
-//   if (user.tokenType === 'Bearer') {
-//     options.headers.Authorization = 'Bearer '+user.accessToken
-//   }
-//   const res = await fetch(url, options)
-//   const json = await res.json()
-//   if (json.error) throw json.error
-//   return json
-// }
+async function fetchYT(options) {
+  let url = options.url
+  if (options.query) {
+    url += '?'
+    for (const key in options.query) {
+      url += key+'='+options.query[key]+'&'
+    }
+  }
+  if (!options.headers) options.headers = {}
+  // if (user.tokenType === 'Bearer') {
+  //   options.headers.Authorization = 'Bearer '+user.accessToken
+  // }
+  const res = await fetch(url, options)
+  const json = await res.json()
+  if (json.error) throw json.error
+  return json
+}
 
 let store = {
+  apiKey: '',
   instances: [
     {
       email: 'test@example.com',
@@ -77,14 +78,22 @@ let store = {
 const wss = new WebSocket.Server({ noServer: true })
 let connections = 0
 
-wss.on('connection', (ws) => {
-  connections += 1
-  console.log(`WS Open (${connections} now open)`)
-  send('newStore', store)
+function sendAll(type, data) {
+  wss.clients.forEach(function each(ws) {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify({ type, data }))
+    }
+  })
+}
 
+wss.on('connection', (ws) => {
   function send(type, data) {
     ws.send(JSON.stringify({ type, data }))
   }
+
+  connections += 1
+  console.log(`WS Open (${connections} now open)`)
+  send('newStore', store)
 
   ws.on('close', () => {
     connections -= 1
@@ -100,11 +109,52 @@ wss.on('connection', (ws) => {
     const { type, data } = JSON.parse(msg)
     console.log(`WS "${type}" message received:`, data)
 
-    if (type === 'newEmail') {
-      if (!data.channels) data.channels = []
-      if (!data.timeLastSynced) data.timeLastSynced = new Date().getTime()
-      store.instances.push(data)
-      ws.send(JSON.stringify({ type: 'initStore', data: store }))
+    try {
+      if (type === 'setApiKey') {
+        store.apiKey = data.key
+        sendAll('newStore', store)
+
+      } else if (type === 'newEmail') {
+        if (!data.channels) data.channels = []
+        if (!data.timeLastSynced) data.timeLastSynced = new Date().getTime()
+        store.instances.push(data)
+        sendAll('newStore', store)
+
+      } else if (type === 'addChannel') {
+        const segments = data.channel.split('/')
+        const channelIdSegment = segments.indexOf('channel') + 1
+        const usernameSegment = segments.indexOf('user') + 1
+          || segments.indexOf('www.youtube.com') + 1
+          || segments.indexOf('youtube.com') + 1
+        const query = { key: store.apiKey, part: 'contentDetails,id,snippet' }
+        if (channelIdSegment > 0) {
+          const channelId = segments[channelIdSegment]
+          query.id = channelId
+        } else if (usernameSegment >= 0) {
+          const username = segments[usernameSegment]
+          query.forUsername = username
+        } else {
+          console.log('unknown', data.channel)
+          throw new Error(`Can't deal with url '${data.channel}'. The url should have /channel/ or /user/ in it.`)
+        }
+        const result = await fetchYT({
+          url: 'https://www.googleapis.com/youtube/v3/channels',
+          query: query,
+        })
+        if (!result.items) throw new Error(`Channel '${data.channel}' not found`)
+        const channelObject = result.items[0]
+        store.instances[data.instance].channels.push({
+          id: channelObject.id,
+          name: channelObject.snippet.title,
+          icon: channelObject.snippet.thumbnails.medium.url,
+          uploadsPlaylistId: channelObject.contentDetails.relatedPlaylists.uploads,
+        })
+        sendAll('newStore', store)
+      }
+    } catch(err) {
+      console.log(':::err:::')
+      console.log(err)
+      send('error', err instanceof Error ? err.toString() : err)
     }
   })
 
--- src/web/index.js
+++ src/web/index.js
@@ -7,7 +7,8 @@ ws.onopen = () => {
 }
 
 ws.onerror = (err) => {
-  console.log('ws ERR')
+  console.log('ws ERR:')
+  console.log(err)
   $('#offline-modal').addClass('is-active')
 }
 ws.onclose = () => {
@@ -15,11 +16,21 @@ ws.onclose = () => {
   $('#offline-modal').addClass('is-active')
 }
 
+let doneFunc
+
 function render(data) {
   const source = document.getElementById('app-template').innerHTML
   const template = window.Handlebars.compile(source)
   const html = template(data)
   document.getElementById('app').innerHTML = html
+
+  $('#api-key-form').submit((e) => {
+    e.preventDefault()
+    window.a('setApiKey', {
+      key: $('#api-key-input').val(),
+    })
+    return false
+  })
 
   const datetime = $('#new-email-datetime')
   datetime.val(new Date().toLocaleString())
@@ -32,6 +43,20 @@ function render(data) {
       timeLastSynced: time,
     })
     $('#new-email-modal').removeClass('is-active')
+    return false
+  })
+
+  $('#add-channel-form').submit((e) => {
+    e.preventDefault()
+    $('#add-channel-modal button[type="submit"]').addClass('is-loading')
+    window.a('addChannel', {
+      instance: $('#add-channel-select-instance select').val(),
+      channel: $('#add-cannel-input').val(),
+    })
+    doneFunc = () => {
+      $('#add-channel-modal button[type="submit"]').removeClass('is-loading')
+      $('#add-channel-modal').removeClass('is-active')
+    }
     return false
   })
 }
@@ -47,8 +72,14 @@ ws.onmessage = (e) => {
   const { type, data } = JSON.parse(e.data)
 
   if (type === 'newStore') {
+    if (doneFunc) doneFunc()
     console.log(data)
     render(data)
+  } else if (type === 'error') {
+    if (doneFunc) doneFunc()
+    console.log('error from server:', data)
+    $('#error-modal pre').text(JSON.stringify(data, null, '  '))
+    $('#error-modal').addClass('is-active')
   }
 
 }
