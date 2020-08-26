8e16a83cf0df9dce893521237c2a094f750ed4f0
Kasper <kasperkh.kh@gmail.com>
2020-08-26 06:17:15 +0200

    Fixed incorrect path

--- src/paths.js
+++ src/paths.js
@@ -8,7 +8,7 @@ if (process.env.APP_ENV === 'dev') {
   paths.dataDir = path.join(__dirname, '../appfiles/space.kasper.youtube-email-notifier')
   paths.settings = path.join(__dirname, '../appfiles/space.kasper.youtube-email-notifier/settings.json')
 } else {
-  paths.dataDir = path.join(__dirname, '~/Library/Application Support/space.kasper.youtube-email-notifier')
+  paths.dataDir = '~/Library/Application Support/space.kasper.youtube-email-notifier'
   paths.settings = path.resolve(paths.dataDir, 'settings.json')
 }
 
